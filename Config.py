bed1c7614ce25c395fd7e107b8c529dc03c57e33
HabDV <99927628+HabDV@users.noreply.github.com>
2022-05-20 22:12:17 +1000

    Update Config.py

--- Config.py
+++ Config.py
@@ -24,11 +24,11 @@ class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        "**Принудительная подписка**\n__\nЯ выдам мут пользователям, если они не присоединились к вашему каналу, и заставлю их присоединиться, после успешной подписки они смогут снять ограничение через кнопку и общаться в чате.__",
         
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        "**Установка**\n__Прежде всего, добавьте меня в группу как администратора с правом на блокировку пользователей и в канал как администратора.\nВажно: Только создатель группы может настроить меня, в противном случаи покину чат, если я не являюсь администратором.__",
         
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        "**Команды**\n__/ForceSubscribe - Чтобы получить текущие настройки.\n/ForceSubscribe no/off/disable - Отключить принудительную подписку\n/ForceSubscribe {channel username} - Для включения и настройки канала.\n/ForceSubscribe clear - Снять блокировку со всех пользователей, которые были отключены мной.\n\nПримечание: /FSub является псевдонимом /ForceSubscribe__",
         
         "**Bot admin @ig_ovosh**"
       ]
